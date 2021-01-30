462cad1eedadc9cd8baa2e54e65c0c1a5b4daa18
Ahmad <myvirtualrabbit15@gmail.com>
2021-01-31 03:59:19 +0600

    27

--- config/database.js
+++ config/database.js
@@ -29,10 +29,10 @@ module.exports = ({ env }) => {
     connections: {
       default: {
         connector: "bookshelf",
-        // settings: {
-        //   client: "sqlite",
-        //   filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-        // },
+        settings: {
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        },
         options: {
           useNullAsDefault: true,
         },
